8611fc0751c82f20b09958c14dab935c47663522
Shveder <shvedegor7687@gmail.com>
2024-08-19 23:50:18 +0300

    Add master: add xml documentation to model

--- Rates/Rates.Core/Models/CurrencyRate.cs
+++ Rates/Rates.Core/Models/CurrencyRate.cs
@@ -5,12 +5,33 @@ namespace Rates.Core.Models;
 
 public class CurrencyRate : IModels
 {
+    /// <summary>
+    /// Id of rate.
+    /// </summary>
     [Key]public Guid Id { get; set; }
+    /// <summary>
+    /// Id of currency.
+    /// </summary>
     [Required]  public int Cur_ID { get; set; }
+    /// <summary>
+    /// Date of rate.
+    /// </summary>
     [Required] public DateTime Date { get; set; }
+    /// <summary>
+    /// Abbreviation of currency.
+    /// </summary>
     [Required] public string Cur_Abbreviation { get; set; }
+    /// <summary>
+    /// Scale of currency.
+    /// </summary>
     [Required] public int Cur_Scale { get; set; }
+    /// <summary>
+    /// Name of currency.
+    /// </summary>
     [Required] public string Cur_Name { get; set; }
+    /// <summary>
+    /// Official rate of currency.
+    /// </summary>
     [Required] public decimal Cur_OfficialRate { get; set; }
 
 }
